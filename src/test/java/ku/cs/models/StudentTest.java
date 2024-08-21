34f45b46c7620ecddce1cb4d4c7dfd2295ba04cf
IsareeJun <isaree.ju@ku.th>
2024-08-21 14:59:00 +0700

    first commit in class

---
+++ src/test/java/ku/cs/models/StudentTest.java
@@ -0,0 +1,57 @@
+package ku.cs.models;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StudentTest {
+
+    Student s1;
+
+    @BeforeEach
+    void init(){
+        s1 = new Student("6610451257","Front");
+    }
+
+    @Test
+    @DisplayName("เทสเพิ่มคะแนน")
+    void testAddScore(){
+        //Student s1 = new Student("6610451257","Front");
+        assertEquals(0, s1.getScore());
+        s1.addScore(40.5);
+        assertEquals(40.5, s1.getScore());
+        s1.addScore(10);
+        assertEquals(50.5, s1.getScore());
+    }
+
+    @Test
+    @DisplayName("เทสเกรด")
+    void testCalculateGrade(){
+        //Student s = new Student("6610451257", "Front");
+        s1.addScore(85);
+        assertEquals("A", s1.grade());
+    }
+
+    @Test
+    @DisplayName("เทส getName")
+    void testGetName(){
+        //Student s = new Student("6610451257", "Front");
+        assertEquals("Front", s1.getName());
+    }
+
+    @Test
+    @DisplayName("เทส getScore")
+    void testGetScore(){
+        assertEquals(0, s1.getScore());
+    }
+
+    @Test
+    @DisplayName("เทสเปลี่ยนชื่อ")
+    void testChangeName(){
+        s1.changeName("fif");
+        assertEquals("fif", s1.getName());
+    }
+
+}
